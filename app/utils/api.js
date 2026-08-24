daf873dd5d42ca0d5e38e980bae84f2e5ac6d551
Basem Mahmoud <basem556473@gmail.com>
2026-08-24 12:20:53 +0300

    fiqhw

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // إنشاء instance
 const api = axios.create({
-    baseURL: 'https://hemma-e.up.railway.app',
+    baseURL: '',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
