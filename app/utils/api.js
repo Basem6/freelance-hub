fbace9abafc9d64fc4be73d9c67567e225006c88
Basem Mahmoud <basem556473@gmail.com>
2026-08-24 12:24:33 +0300

    fgwp

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,11 +2,18 @@ import axios from 'axios';
 
 // إنشاء instance
 const api = axios.create({
-    baseURL: '',
+    baseURL: '/api/backend',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     } 
+});
+
+api.interceptors.request.use((config) => {
+    if (config.url?.startsWith('/api/auth/')) {
+        config.baseURL = '';
+    }
+    return config;
 });
 
 export default api;
